9c7067309e38eed965283e75176759e226fec267
Farhinkhan <100411619+Farhinkhan@users.noreply.github.com>
2022-05-07 15:35:00 +0530

    Update Greatest_no..py

--- Greatest_no..py
+++ Greatest_no..py
@@ -3,10 +3,6 @@ b=int(input("Enter second number:"))
 c=int(input("Enter third number:"))
 if (a==b==c):
    print("All Numbers are equal")
-elif(a==b and a>c):
-    print("a,b are equal and c is small")
-elif(b==c and b>a):
-    print("b,c  are equal and a  is small") 
 elif(a>b)and (a>c):
    print(a,"is greater than", b ,"and",c)
 elif(b>a) and (b>c):
