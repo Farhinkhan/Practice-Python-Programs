41fa94c0ee11da68a68fd3f09eee105f587f293b
Farhinkhan <100411619+Farhinkhan@users.noreply.github.com>
2022-05-07 15:42:53 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,14 @@
+a= int(input("please enter a number:"))
+#a=float(a)
+print(type(a))
+print(a%2)
+if(a==0):
+    print(a ,"is neither even nor odd")
+elif(a%2==0):
+    print(a,"is Even Number")
+else:
+    print(a,"is Odd Number.0")
+
+
+7
+
