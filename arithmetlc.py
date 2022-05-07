0218ee708a5e6b1042091df831588bb7c07b4767
Farhinkhan <100411619+Farhinkhan@users.noreply.github.com>
2022-05-07 16:08:29 +0530

    Add files via upload

---
+++ arithmetlc.py
@@ -0,0 +1,29 @@
+#Program to perfrom Arithmetic Operations
+a=98
+b=54
+c=a+b
+print(c)
+
+a=4
+b=2
+c=a/b
+print(c)
+    #floor division
+a=5.4
+b=2
+c=a//b
+print(c)
+
+a=3
+b=3
+c=a**b
+print(c)
+
+a=6
+print(a)
+a-=5
+print(a)
+
+a=5
+b=6
+print(b>a)
