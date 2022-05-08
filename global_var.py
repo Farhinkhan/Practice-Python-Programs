24b78af7736b022efd1a2e7d9f6f14e4f1d1cb16
Farhinkhan <100411619+Farhinkhan@users.noreply.github.com>
2022-05-08 22:10:03 +0530

    Add files via upload

---
+++ global_var.py
@@ -0,0 +1,15 @@
+'''def user_input():
+    a=int(input("Enter Value 1 : "))
+    b=int(input("Enter value 2 : "))
+    return(a,b)
+def add(a,b):
+    return(a+b)
+a,b=user_input()
+add(a,b)
+print(a,'+',b,"=",add(a,b))'''  
+
+
+
+a,b=input("Enter value1:"),input("Enter value2 : ")
+print(a,"\n",b)
+
