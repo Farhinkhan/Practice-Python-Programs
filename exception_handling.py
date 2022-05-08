8ad75079338e4dc128fbd86c97ef5146a3457745
Farhinkhan <100411619+Farhinkhan@users.noreply.github.com>
2022-05-09 00:46:27 +0530

    Add files via upload

---
+++ exception_handling.py
@@ -0,0 +1,14 @@
+#try except finally 
+def function():
+	pass
+try : 
+	a = int(input("Enter a Value : "))
+	b = int(input("Enter a Value : "))
+	c = a/b 
+	print("{}/{}={}".format(a,b,c))
+
+except ZeroDivisionError :
+	print("Denominator can't be zero")
+
+except ValueError : 
+	print("Please Provide a Integer value only")
