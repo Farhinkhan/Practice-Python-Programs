865b2ee3276bd43670fb9d6062e76073f74e90cf
Farhinkhan <100411619+Farhinkhan@users.noreply.github.com>
2022-05-08 22:24:32 +0530

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,27 @@
+#slicing
+i=input("Enter a string:")
+x=i[3:9]
+print(x)
+#x=i[ :3]
+x=i[2: ]
+print(i)
+print(x)
+
+#spliting
+a=input("Enter a string:")
+x=a.split(" ")
+print(x)
+
+#concatenation
+x=input("Enter a string:")
+y=input("Enter a  another string:")
+z=x + " " + y
+print(z)
+
+#Multiplication
+
+x=input("Enter a string:")
+y=int(input("Enter number:"))
+z=x * y
+print(z)
+
