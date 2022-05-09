741eb3a83ad391b4dfa164081c19e8ddf501a731
Farhinkhan <100411619+Farhinkhan@users.noreply.github.com>
2022-05-09 13:58:36 +0530

    Add files via upload

---
+++ celcius_fahrenheit.py
@@ -0,0 +1,6 @@
+#Program to convert celsius to fahrenheit
+celsius = int(input("Enter the value:"))
+fahrenheit = (celsius * 1.8) + 32
+ 
+print('%.2f Celsius is equivalent to: %.2f Fahrenheit'
+      %(celsius, fahrenheit))
